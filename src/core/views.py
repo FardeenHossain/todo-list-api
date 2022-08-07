9f6daf4faa50c160d933bd6c06f46f249f93e15b
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-08-07 14:12:35 +0100

    Added ToDo viewset

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import permissions, viewsets
+from core.models import ToDo
+from core.serializers import ToDoSerializer, UserSerializer
 
-# Create your views here.
+
+class ToDoViewSet(viewsets.ModelViewSet):
+    """This viewset automaitcally provides create, read, update, and delete actions."""
+
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
