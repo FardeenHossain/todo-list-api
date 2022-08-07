f4a949c65ca575a1063746c6d60b337e148b7d32
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-08-07 14:04:51 +0100

    Added ToDo model

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    owner = models.ForeignKey('auth.User', related_name='todo', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created', )
