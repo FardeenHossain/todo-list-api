eb0106ebffeab380bc7d6be5ec3f49f7b19109e4
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-08-07 14:10:25 +0100

    Added user serializer

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,4 +1,3 @@
-from dataclasses import field
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from core.models import ToDo
@@ -10,3 +9,11 @@ class ToDoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ToDo
         fields = ['url', 'id', 'owner', 'title', 'completed']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    todo = serializers.HyperlinkedRelatedField(many=True, view_name='todo-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'todo']
